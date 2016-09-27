734163a48c1ec49b63ed57482224b144902fc12e
¨Scott <scott.glover@r1soft.com>
2016-09-27 17:47:06 -0500

    Added pack and unpack options.

--- img4ramdisk.c
+++ img4ramdisk.c
@@ -3,28 +3,31 @@
 int main(int argc, char **argv) {
 	char buf[512];
 	char filename[255];
-	FILE *fin, *fout1, *fout2;
 	size_t count;
 
-	if (argc < 2) {
-		printf("Usage: %s <ramdisk_name>.dmg\n", argv[0]);
+	if (argc < 3) {
+		printf("Usage: %s <unpack|pack> <ramdisk_name>.dmg\n", argv[0]);
 		return 0;
 	}
-	strcpy(filename, argv[1]);
+
+	if (!strcmp(argv[1], "unpack")) {
+	FILE *fin, *fout1, *fout2;
+
+	strcpy(filename, argv[2]);
 	strcat(filename, ".dmg");
 
 	printf("Opening %s\n", filename);
 	fin = fopen(filename, "rb");
 	fread(buf, 1, 27, fin);
 
-	strcpy(filename, argv[1]);
+	strcpy(filename, argv[2]);
 	strcat(filename, ".hdr");
 	fout1 = fopen(filename, "wb");
 	fwrite(buf, 1, 27, fout1);
 	fclose(fout1);
 	printf("Created %s\n", filename);
 
-	strcpy(filename, argv[1]);
+	strcpy(filename, argv[2]);
 	strcat(filename, ".raw.dmg");
 	fout2 = fopen(filename, "wb");
 
@@ -35,4 +38,31 @@ int main(int argc, char **argv) {
 	fclose(fout2);
 	printf("Created %s\n", filename);
 	fclose(fin);
+	} else if (!strcmp(argv[1], "pack")) {
+	FILE *fin1, *fin2, *fout;
+        strcpy(filename, argv[2]);
+        strcat(filename, ".dmg");
+
+        fout = fopen(filename, "wb");
+
+        strcpy(filename, argv[2]);
+        strcat(filename, ".hdr");
+        fin1 = fopen(filename, "rb");
+        fread(buf, 1, 27, fin1);
+	fwrite(buf, 1, 27, fout);
+        fclose(fin1);
+
+        strcpy(filename, argv[2]);
+        strcat(filename, ".raw.dmg");
+        fin2 = fopen(filename, "rb");
+
+        while (!feof(fin2)) {
+                count = fread(buf, 1, 512, fin2);
+                fwrite(buf, 1, count, fout);
+        }
+        fclose(fout);
+        fclose(fin2);
+
+
+	}
 }
