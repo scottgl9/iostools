a6761d80dce441f3be2a7a990fa9989155509e4e
¨Scott <scott.glover@r1soft.com>
2016-10-28 15:01:30 -0500

    Updated img4ramdisk tool to update size of .raw.dmg in img4 header.

--- img4ramdisk.c
+++ img4ramdisk.c
@@ -1,4 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#define OFF1 0x02
+#define OFF2 0x17
+
+#ifndef uint8_t
+#define uint8_t unsigned char
+#endif
+
+// at offsets 0x02 and 0x17 (3 byte little endian length of .raw.dmg): 02 73 70
+void header_update_size(char *buf, char *filename) {
+	size_t size=0;
+	struct stat st;
+
+	if (stat(filename, &st) == -1) {
+		printf("Failed to stat file %s\n", filename);
+		return;
+	}
+	size = st.st_size;
+
+	//b[0] = (uint8_t) (size >>  0u);
+	buf[OFF1+2] = (uint8_t) (size >>  8u);
+	buf[OFF1+1] = (uint8_t) (size >> 16u);
+	buf[OFF1] = (uint8_t) (size >> 24u);
+
+	buf[OFF2+2] = (uint8_t) (size >>  8u);
+	buf[OFF2+1] = (uint8_t) (size >> 16u);
+	buf[OFF2] = (uint8_t) (size >> 24u);
+
+	printf("Updated size to: %.2X %.2X %.2X (size=%X)\n", buf[OFF2]&0xFF, buf[OFF2+1]&0xFF, buf[OFF2+2]&0xFF, size);
+}
 
 int main(int argc, char **argv) {
 	char buf[512];
@@ -54,11 +87,16 @@ int main(int argc, char **argv) {
         strcat(filename, ".hdr");
         fin1 = fopen(filename, "rb");
         fread(buf, 1, 27, fin1);
+
+	strcpy(filename, argv[2]);
+	strcat(filename, ".raw.dmg");
+
+	// at offsets 0x02 and 0x17 (3 byte big endian length of .raw.dmg): 02 73 70
+	header_update_size(buf, filename);
+
 	fwrite(buf, 1, 27, fout);
         fclose(fin1);
 
-        strcpy(filename, argv[2]);
-        strcat(filename, ".raw.dmg");
         fin2 = fopen(filename, "rb");
 
         while (!feof(fin2)) {
